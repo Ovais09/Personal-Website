0393cc835452fb3a56f2fe1a349298374e546bd9
Ovais09 <ovazeem100@gmail.com>
2022-12-31 14:14:47 -0500

    installed bootstrap button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 
-
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 export default function Home() {
   return (
     <>
-     <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+      <Button variant="outline-primary">Primary</Button>
     </>
   )
 }
