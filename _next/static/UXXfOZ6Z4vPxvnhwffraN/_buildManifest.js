a3dd62a63e5259773746942a7050be687a6de7d8
Ovais09 <ovazeem100@gmail.com>
2022-12-31 04:00:35 -0500

    Updates

---
+++ _next/static/UXXfOZ6Z4vPxvnhwffraN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4f00d2ffb91b44dc.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
