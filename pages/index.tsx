6a2b5dda26dc6b1baea5d1ca5bbed30c703a2fdc
Ovais09 <ovazeem100@gmail.com>
2022-12-31 15:59:10 -0500

    firebase hosting

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export default function Home() {
   return (
     <>
-      <h1>Hello World!!!!!</h1>
+      <h1>Hello World!</h1>
       <Button variant="primary">Primary</Button>
     </>
   )
