a8ea58a8b9afafbc03ba4ccb8f2defbf44e82cef
Ovais09 <ovazeem100@gmail.com>
2023-01-08 16:15:07 -0500

    made it into a PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,9 @@ export default function Document() {
         <link rel="shortcut icon" href="/logo.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
